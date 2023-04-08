18d703ee98e30401a87809a648ac51fc75170c03
阿吕 <1251858997@qq.com>
2023-04-09 02:32:34 +0800

    add gpt接口

--- src/main/java/com/yupi/autoreply/controller/MainController.java
+++ src/main/java/com/yupi/autoreply/controller/MainController.java
@@ -1,7 +1,13 @@
 package com.yupi.autoreply.controller;
 
+import com.yupi.autoreply.answerer.Answerer;
+import com.yupi.autoreply.answerer.OpenAiAnswerer;
+import com.yupi.autoreply.common.BaseResponse;
+import com.yupi.autoreply.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
@@ -11,8 +17,20 @@ import org.springframework.web.bind.annotation.*;
  * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/openai")
 @Slf4j
 public class MainController {
+
+    @GetMapping("/quiz")
+    public BaseResponse<String> quziAnswer(String quizContext) {
+        try {
+            Answerer answerer = new OpenAiAnswerer();
+            String answer = answerer.doAnswer(quizContext);
+            return ResultUtils.success(answer);
+        } catch (Exception e) {
+            log.error("【提问异常】msg = {}, e = {}", e.getMessage(), e);
+            return ResultUtils.error(500, e.getMessage());
+        }
+    }
 
 }
